906c4c8579fc3f6606698fcfee137a6d45e617a8
huhao <nepood@gmail.com>
2017-12-22 12:41:45 +0800

    add auto update support for ctags and cscope

--- ctags_cscope_update.sh
+++ ctags_cscope_update.sh
@@ -1,44 +1,86 @@
 #!/usr/bin/env bash
 
-echo ""
+update_ctags_cscope()
+{
+    echo ""
 
-# delete old tags and cscope files
-if [ -e "tags" ]
-then
-    rm tags
-fi
-
-cscope_item=( ./cscope.out ./cscope.in.out ./cscope.po.out )
-for item in ${cscope_item[@]}
-do
-    if [ -f $item ]
+    # generate ctags
+    ctags -Rnf ./tags.new
+    # delete old tags and cscope files
+    if [ -e "tags" ]
     then
-        rm $item
+        cp tags tags.old
+        mv tags.new tags
     fi
-done
 
-echo "Update code tags"
+    rm cscope*
+    # generate cscope
+    cscope -Rbkq
 
-# generate ctags
-ctags -Rn
-# check whether tags file generated
-find ./ -name "tags" > /dev/null
-if [ $? -eq 0 ]
-then
-    echo "...ctags built"
-else
-    echo "Fail to build ctags"
-fi
+    echo "Update code tags"
 
-# generate cscope
-cscope -Rbkq
-# check how many cscope file generated
-cs_num=$(find ./ -name "cscope*" | wc -l)
-if [ $cs_num -eq 3 ]
-then
-    echo "...cscope built"
-else
-    echo "Fail to build cscope"
-fi
+    # check whether tags file generated
+    find ./ -name "tags" > /dev/null
+    if [ $? -eq 0 ]
+    then
+        echo "...ctags built"
+    else
+        echo "Fail to build ctags"
+    fi
 
-echo "Finish update code tags, let's roll"
+    # check how many cscope file generated
+    cs_num=$(find ./ -iname "cscope*" | wc -l)
+    if [ $cs_num -eq 3 ]
+    then
+        echo "...cscope built"
+    else
+        echo "Fail to build cscope"
+    fi
+
+    echo "Finish update code tags, let's roll"
+}
+
+check_vim_status()
+{
+    # if vim is on, return 0, otherwise return 1
+    vim_on=$(ps aux | grep vim)
+}
+
+main()
+{
+    # first generate tags and cscope in current directory
+    update_ctags_cscope
+    # use a counter to record vim inactive time
+    local cnt="0"
+    # generate tags and cscope files every 10 second
+    while true
+    do
+        # check whether vim is active
+        check_vim_status
+        # 0: on, 1: off
+        vim_status=$?
+        # vim is active, update tags every 10 second
+        if [ $vim_status -eq 0 ]
+        then
+            # update tags and copy to work directory
+            update_ctags_cscope
+        # vim is inactive, stop process
+        else
+            # wait for 60 second for vim re-active
+            if [ $cnt -gt 6 ]
+            then
+                # or end process
+                echo "I need rest, goodbye"
+                rm tags* cscope*
+                break
+            else
+                echo "vim is not in process"
+                cnt=$[$cnt+1]
+            fi
+        fi
+        sleep 10
+    done
+}
+
+# run in background slient
+main $@ > /dev/null &
