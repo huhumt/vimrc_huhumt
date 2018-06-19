a04485f2b40ec92680ddf4aea7613e3101036681
huhao <nepood@gmail.com>
2018-06-19 14:09:45 +0800

    auto replace default mingw mirror with local USTC and TSINGHUA mirror

--- auto_install.sh
+++ auto_install.sh
@@ -4,8 +4,16 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 mkdir ~/.vim/plugged
 cp .vimrc .tmux.conf ~/
-vim -c "silent PlugInstall" -c "qa"
+vim -c "silent PlugClean" -c "silent PlugInstall" -c "qa"
 
-cp ./demo/mirrorlist* /etc/pacman.d/
-cp ./cn_update_package /usr/local/bin/
-cn_update_package
+# get system info of kernal and os in lowercase
+kernel_name=$(echo "$(uname -s)" | tr '[:upper:]' '[:lower:]')
+os_name=$(echo "$(uname -o)" | tr '[:upper:]' '[:lower:]')
+if [[ "$kernel_name" = *"mingw"* ]] || [[ "$os_name" = *"msys"* ]]
+then
+    sed -i 's/'"\/usr\/bin\/ctags"'/'"\/mingw64\/bin\/ctags"'/g' ~/.vimrc
+    # use local mirror to speedup download
+    cp ./demo/mirrorlist* /etc/pacman.d/
+    cp ./cn_update_package /usr/local/bin/
+    cn_update_package
+fi
