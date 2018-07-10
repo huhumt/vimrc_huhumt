959b7643678f7e94331bdfaeca215a4e80b9275e
huhumt <nepood@gmail.com>
2018-07-10 17:34:15 +0800

    make it easier to auto install all the script

--- auto_install.sh
+++ auto_install.sh
@@ -1,22 +1,44 @@
-#/bin/bash
+#!/usr/bin/env bash
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+# get current user's home directory
+home_director=$(eval echo ~${SUDO_USER})
+
+# use vim-plug to manage all vim plugins
+curl -fLo $home_director/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-mkdir ~/.vim/plugged
-cp .vimrc .tmux.conf ~/
+mkdir -p $home_director/.vim/plugged
+# use self-configuration for vim and tmux
+cp .vimrc .tmux.conf $home_director
+# install necessary vim plugins automatic
 vim -c "silent PlugClean" -c "silent PlugInstall" -c "qa"
+hexmode_plugin_directory="$home_director/.vim/plugged/hexmode/plugin/hexmode.vim"
+# replace the first xxd command to xxd -g1, print one byte instead of two per group
+sed -i '0,/silent %!xxd/ s/silent %!xxd/silent %!xxd -g1/' $hexmode_plugin_directory
 
-mkdir ~/.config/pip
-cp pip.conf ~/.config/pip/
+# use ustc mirror to speed up python-pip
+mkdir -p $home_director/.config/pip
+cp pip.conf $home_director/.config/pip/
 
+usr_local_bin_path="/usr/local/bin"
 # get system info of kernal and os in lowercase
 kernel_name=$(echo "$(uname -s)" | tr '[:upper:]' '[:lower:]')
 os_name=$(echo "$(uname -o)" | tr '[:upper:]' '[:lower:]')
 if [[ "$kernel_name" = *"mingw"* ]] || [[ "$os_name" = *"msys"* ]]
 then
-    sed -i 's/'"\/usr\/bin\/ctags"'/'"\/mingw64\/bin\/ctags"'/g' ~/.vimrc
+    # sed -i 's/'"\/usr\/bin\/ctags"'/'"\/mingw64\/bin\/ctags"'/g' ~/.vimrc
     # use local mirror to speedup download
     cp ./demo/mirrorlist* /etc/pacman.d/
     cp ./cn_update_package /usr/local/bin/
     cn_update_package
+else
+    usr_local_bin_path="$home_director/.local/bin"
+    mkdir -p $usr_local_bin_path
+    cp $home_director/.bash_profile $home_director/.bash_profile.bak
+    echo "PATH=$PATH:$usr_local_bin_path" >> $home_director/.bash_profile
 fi
+
+cp ./code_backup.sh \
+   ./ctags_cscope_update.sh \
+   ./replace_line.py \
+   ./code_format_clang.sh \
+   $usr_local_bin_path
