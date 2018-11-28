8dd0daae3c4594cbd8569be65c656dd801c60b6f
huhumt <nepood@gmail.com>
2018-11-28 14:24:12 +0800

    use today data label to copy target bin

--- keil_compile.sh
+++ keil_compile.sh
@@ -1,16 +1,40 @@
 #!/usr/bin/env bash
 
-cc="C:\Program Files (x86)\Keil\C251\UV4\Uv4.exe"
-target=$(find ../ -iname "*.uvproj")
-compile_project="\"$cc\" -b \"$target\" -o output.txt"
+get_date()
+{
+    year=$(date +%Y)
+    month=$(date +%m)
+    day=$(date +%d)
+    echo "$year$month$day"
+}
 
-eval $compile_project
-mv ../mcu/keil/output.txt ./compile.log
+compile_project()
+{
+    cc="C:\Program Files (x86)\Keil\C251\UV4\Uv4.exe"
+    target=$(find ../ -iname "*.uvproj")
+    compile_project="\"$cc\" -b \"$target\" -o output.txt"
+    eval $compile_project
+}
 
-latest_bin=$(ls -t ../mcu/output/*Ref*app.bin | head -1)
-check_bin=$(find ./ -name "*app.bin" | wc -l)
-if [ $check_bin -gt 0 ]
-then
-    rm -f ./*app.bin
-fi
-cp "$latest_bin" ./
+copy_log_and_bin()
+{
+    mv ../mcu/keil/output.txt ./compile.log
+
+    today_date=$(get_date)
+    target_bin="../mcu/output/*""$today_date""_app.bin"
+    latest_bin=$(ls -t $target_bin | head -1)
+    check_bin=$(find ./ -name "*app.bin" | wc -l)
+    if [ $check_bin -gt 0 ]
+    then
+        rm -f ./*app.bin
+    fi
+    cp "$latest_bin" ./
+}
+
+main()
+{
+    compile_project
+    copy_log_and_bin
+}
+
+main
