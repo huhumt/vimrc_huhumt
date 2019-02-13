3034ffe7bc88fd9adbf25818f86f78f9e1a0b9a5
huhumt <nepood@gmail.com>
2019-02-13 15:07:11 +0800

    add support to auto install youcompleteme, but fail at last >_<

--- auto_install_Youcompleteme.sh
+++ auto_install_Youcompleteme.sh
@@ -18,10 +18,21 @@ cd ~/YouCompleteMe
 git submodule update --init --recursive
 mkdir build
 cd build
-cmake -G "Unix Makefiles" \
+
+curl -fLo ../third_party/ycmd/cpp/BoostParts/boost/detail/interlocked.hpp \
+    https://raw.githubusercontent.com/Reikion/YouCompleteMe/master/cpp/BoostParts/boost/detail/interlocked.hpp
+
+echo "
+set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -include cmath")
+add_definitions(-DBOOST_PYTHON_SOURCE)
+add_definitions(-DBOOST_THREAD_BUILD_DLL)
+add_definitions(-DMS_WIN64)" >> ../third_party/ycmd/cpp/CMakeLists.txt
+
+cmake -G "MSYS Makefiles" \
     -DPYTHON_LIBRARY=/mingw64/lib/libpython3.7.dll.a \
     -DPYTHON_INCLUDE_DIR=/mingw64/include/python3.7m \
     -DEXTERNAL_LIBCLANG_PATH=/mingw64/lib/libclang.dll.a \
-    -DUSE_CLANG_TIDY=ON -DUSE_PYTHON2=OFF . ~/YouCompleteMe/third_party/ycmd/cpp
+    -DUSE_CLANG_TIDY=OFF -DUSE_PYTHON2=OFF . ../third_party/ycmd/cpp
 make ycm_core
-make ycm_support_libs
+
+cp ../third_party/ycmd/ycm_core.pyd ../python/
