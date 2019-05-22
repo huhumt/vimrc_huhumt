a7ba9ce374755e830a9a5439cff33b572afc1143
huhumt <nepood@gmail.com>
2019-05-22 14:04:32 +0800

    now can search for file

--- keil_compile.sh
+++ keil_compile.sh
@@ -2,9 +2,16 @@
 
 get_date()
 {
-    year=$(date +%Y)
-    month=$(date +%m)
-    day=$(date +%d)
+    if [ "$1" = "" ]
+    then
+        local year=$(date +%Y)
+        local month=$(date +%m)
+        local day=$(date +%d)
+    else
+        local year=$(date +%Y -r $1)
+        local month=$(date +%m -r $1)
+        local day=$(date +%d -r $1)
+    fi
     echo "$year$month$day"
 }
 
@@ -25,7 +32,18 @@ compile_project()
 
 copy_log_and_bin()
 {
-    mv ../mcu/keil/output.txt ./compile.log
+    local today_date=$(get_date)
+    local log_filename=$(find ../ -iname "output.txt")
+    if [ ! -z "$log_filename" -a "$log_filename" != " " ]
+    then
+        mv $log_filename ./compile.log
+        local last_line=$(tail -n1 compile.log)
+        if [[ "$last_line" == *"Target not created"* ]]
+        then
+            printf "\t $today_date: Failed to compile project, refer to 'compile.log' to see details\n"
+            return 1
+        fi
+    fi
 
     local target_bin_list=( _app.bin _Bootloader.bin _Pramboot.bin )
     for target_bin in ${target_bin_list[@]}
@@ -36,23 +54,28 @@ copy_log_and_bin()
             rm -f ./*$target_bin
         fi
 
+        output_dir=$(find ../ -name "output")
         if [ "$taget_bin" = "_app.bin" ]
         then
-            local today_date=$(get_date)
-            local app_bin="../mcu/output/*""$today_date""_app.bin"
+            local app_bin="""$output_dir""/FT*""$today_date""_app.bin"
         else
-            local app_bin="../mcu/output/*""$target_bin"""
+            local app_bin="""$output_dir""/FT*""$target_bin"""
         fi
 
         local check_app_bin=$(ls -l $app_bin 2>/dev/null | wc -l)
         if [ $check_app_bin -gt 0 ]
         then
-            latest_bin=$(ls -t $app_bin | head -1)
-            printf "\tCopy $latest_bin to current direcotry\n"
-            cp "$latest_bin" ./
-            break
+            local latest_bin=$(ls -t $app_bin | head -1)
+            local bin_date=$(get_date $latest_bin)
+            if [ "$bin_date" = "$today_date" ]
+            then
+                printf "\t $bin_date: Success to compile project, copy '$latest_bin' to current direcotry\n"
+                cp "$latest_bin" ./
+                return 0
+            fi
         fi
     done
+    return 2
 }
 
 main()
