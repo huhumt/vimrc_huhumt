610ded70e5a34b585bfef55da02f82f3efa76106
huhumt <nepood@gmail.com>
2019-06-03 16:38:18 +0800

    use dynamic array and format printf style

--- sum_of_code_lines.sh
+++ sum_of_code_lines.sh
@@ -9,8 +9,8 @@ get_file_lines()
 main()
 {
     local support_file_type=(".h" ".hpp" ".c" ".cpp")
-    local total_lines=("0" "0" "0" "0")
-    local total_files=("0" "0" "0" "0")
+    local total_lines=()
+    local total_files=()
     local all_file_lines="0"
     local all_file_num="0"
     local cnt="0"
@@ -18,24 +18,30 @@ main()
     do
         local filename
         local filename_array=$(find "$1" -name "*$type")
+        local type_file_lines="0"
+        local type_file_num="0"
         for filename in ${filename_array[@]}
         do
             local lines=$(get_file_lines "$filename")
-            printf "%s lines of %s file\n" $lines $filename
-            total_lines[cnt]=$((total_lines[cnt] + lines))
-            total_files[cnt]=$((total_files[cnt] + 1))
-            all_file_lines=$((all_file_lines + lines))
-            all_file_num=$((all_file_num + 1))
+            printf "%6s lines of %s file\n" $lines $filename
+            type_file_num=$((type_file_num + 1))
+            type_file_lines=$((type_file_lines + lines))
         done
+
+        all_file_num=$((all_file_num + type_file_num))
+        all_file_lines=$((all_file_lines + type_file_lines))
+
+        total_lines[$cnt]=$type_file_lines
+        total_files[$cnt]=$type_file_num
         cnt=$((cnt + 1))
     done
 
     printf "\nTotal have %u files with %u lines:\n" $all_file_num $all_file_lines
     for i in $( seq 0 $((${#support_file_type[@]} - 1)))
     do
-        if [ ${total_files[i]} -gt 0 ]
+        if [ ${total_files[$i]} -gt 0 ]
         then
-            printf "\t------------%d %s files with %d total lines\n" ${total_files[i]} ${support_file_type[i]} ${total_lines[i]}
+            printf "\t%6d %6s files with %10d total lines\n" ${total_files[$i]} ${support_file_type[$i]} ${total_lines[$i]}
         fi
     done
 }
