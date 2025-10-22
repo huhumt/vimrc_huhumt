9f5b9dd604285a69d2fa507de3f0e13b6fdf58e0
test <test@test.co.uk>
2025-10-22 14:18:08 +0000

    Update neomutt script

--- dotfiles/.config/neomutt/cb_mutt_msg.py
+++ dotfiles/.config/neomutt/cb_mutt_msg.py
@@ -1,126 +1,135 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from collections import OrderedDict
+from datetime import datetime, timedelta
 import subprocess
 import sys
 import os
 import re
 
 
-MAIL_FLAG = "@test.co.uk"
-APPEND_MODE, REWRITE_MODE = range(2)
+class AddressBook:
+    def __init__(self, filename):
+        try:
+            with open(filename, "r", encoding="utf-8") as f:
+                self.address_book = f.read()
+        except FileExistsError as e:
+            print(e)
+        except UnicodeDecodeError as e:
+            print(e)
 
-append_newline_flag = False
-address_book_header = ""
+        self.filename = filename
+        self.address_book_header = ""
+        self.address_book_list = list()
 
+    @property
+    def get_address_book(self):
+        return self.address_book
 
-def read_address_book(filename):
-    """
-    read address_book file to python dict
-    """
+    @property
+    def get_address_book_header(self):
+        return self.address_book_header
 
-    def _search_valid_email(email_string):
-        for s in email_string.split(","):
-            email_address = s.strip()
-            if email_address.endswith(MAIL_FLAG):
-                return email_address
-        return None
+    @property
+    def get_address_book_list(self):
+        return self.address_book_list
 
-    global address_book_header
-    global append_newline_flag
+    def read_address_book(self):
+        """
+        read address_book file to python dict
+        """
 
-    address_book_dict = OrderedDict()
+        re_header = r'(?P<header>^[\s\S]+?)\[0\]'
+        if header := re.findall(re_header, self.address_book):
+            self.address_book_header = header[0]
+        else:
+            raise ValueError("Not a valid addressbook file")
 
-    with open(filename, 'r', encoding="utf-8") as fd:
-        person_dict = OrderedDict()
-        start_flag = False
-        s_line = ""
-        email = ""
+        re_person = r'(?:\[\d+\])(?P<first>(?:\r?\n\w+=[ \S]+)*\r?\n)email=(?P<email>[^,\s]+).*(?P<end>(?:\r?\n\w+=[ \S]+)*)'
+        for first, email, end in re.findall(re_person, self.address_book):
+            if self.validate_email(email):
+                self.address_book_list.append(
+                    (email, f"{first}email={email}{end}")
+                )
 
-        for line in fd:
-            s_line = line.strip()
-            if start_flag and s_line and "=" in s_line:
-                key, val = [" ".join(s.split()) for s in s_line.split("=", 1)]
-                if "email" in key.lower():
-                    email = _search_valid_email(val.lower())
-                    val = email
-                person_dict[key] = val
+    def update_address_book(self, email_list, force_write=False):
+        """
+        update address_book based on email_list
+        """
 
-            if re.search(r"\[([0-9]+)\]", s_line):  # find [1234]
-                start_flag = True
-                if (email and person_dict
-                        and email not in address_book_dict.keys()):
-                    address_book_dict[email] = person_dict
-                person_dict = {}
-                email = ""
-            address_book_header += line if not start_flag else ""
+        def _dict_to_abook_str(abook_list, start_cnt=0):
+            """
+            out abook string from list
+            """
+            two_newline = os.linesep * 2
+            return two_newline.join(
+                [
+                    f"[{start_cnt+i}]{p[1]}"
+                    for i, p in enumerate(sorted(abook_list, key=lambda x: x[0]))
+                ]
+                + [""]
+            )
 
-        append_newline_flag = True if s_line else False
-        if email and person_dict and email not in address_book_dict.keys():
-            address_book_dict[email] = person_dict
+        new_email_list = []
+        for email in email_list:
+            email_alphabet = re.sub("[ .]", "", email.lower())
+            address_book_key_alphabet = [
+                re.sub("[ .]", "", s[0].lower())
+                for s in self.address_book_list
+            ]
 
-    return address_book_dict
+            if email_alphabet not in address_book_key_alphabet:
+                name = email.split("@")[0].replace(".", " ").title()
+                new_email_list.append(
+                    (email, f"{os.linesep}name={name}{os.linesep}email={email}")
+                )
 
+        if force_write:
+            with open(self.filename, "w", encoding="utf-8") as fd:
+                fd.write(self.address_book_header + _dict_to_abook_str(
+                    self.address_book_list + new_email_list
+                ))
+        else:
+            with open(self.filename, "a", encoding="utf-8") as fd:
+                fd.write(_dict_to_abook_str(
+                    new_email_list, start_cnt=len(self.address_book_list)
+                ))
 
-def read_email(email_text):
-    """
-    parse email
-    """
+    @staticmethod
+    def read_email(email_text):
+        """
+        parse email
+        """
+        try:
+            re_date = r'Date: \w+,[ ]+(?P<date>\d+[ ]+\w+[ ]+\d{4})'
+            email_date_re = re.findall(re_date, email_text)
+            email_date = datetime.strptime(email_date_re[0], '%d %b %Y').date()
+            one_month_ago_date = (datetime.now() - timedelta(days=30)).date()
+            if email_date > one_month_ago_date:
+                re_email = r'[\w.+-]+@[\w-]+\.[\w.-]+'
+                return list(
+                    f for f in set(re.findall(re_email, email_text))
+                    if AddressBook.validate_email(f)
+                )
+            else:
+                return list()
+        except:
+            return list()
 
-    email_list = []
-    # for line in email_text.splitlines():
-    for line in email_text.splitlines():
-        line_strip = line.lower().strip()
-        if ((line_strip.startswith("from:")
-             or line_strip.startswith("to:"))
-                and MAIL_FLAG in line_strip):
-            for s in re.findall(r"[\w.+-]+@[\w-]+\.[\w.-]+", line):
-                if s.endswith(MAIL_FLAG) and s not in email_list:
-                    email_list.append(s)
-    return email_list
+    @staticmethod
+    def validate_email(email_address):
+        """
+        validate email address
+        """
+        return all([
+            "test.co.uk" in email_address,
+            not bool(re.search(r'mr-\d+', email_address))
+        ])
 
-
-def update_address_book(filename, address_book_dict, email_list, mode):
-    """
-    update address_book based on email_list
-    """
-
-    new_string = ""
-    address_book_len = len(address_book_dict.keys())
-    for email in email_list:
-        email_alphabet = re.sub("[ .]", "", email.lower())
-        address_book_key_alphabet = [
-            re.sub("[ .]", "", s.lower()) for s in address_book_dict.keys()
-        ]
-
-        if email_alphabet not in address_book_key_alphabet:
-            new_string += f"[{address_book_len}]\n"
-            name = email.split("@")[0].replace(".", " ").title()
-            new_string += f"name={name}\n"
-            new_string += f"email={email}\n\n"
-            address_book_len += 1
-
-    if mode == REWRITE_MODE:
-        w_string = address_book_header
-        for i, k in enumerate(address_book_dict):
-            w_string += f"[{i}]\n"
-            for key, val in address_book_dict[k].items():
-                w_string += f"{key}={val}\n"
-            w_string += "\n"
-        with open(filename, "w", encoding="utf-8") as fd:
-            fd.write(f"{w_string}{new_string}")
-    else:
-        if new_string:
-            if append_newline_flag:
-                new_string = f"\n{new_string}"
-            with open(filename, "a", encoding="utf-8") as fd:
-                fd.write(new_string)
-
-
-def hyperlinks_text(url, hyper_txt):
-    hyperlinks = '\033]8;{};{}\033\\{}\033]8;;\033\\'
-    return hyperlinks.format("", url, hyper_txt)
+    @staticmethod
+    def hyperlinks_text(url, hyper_txt):
+        hyperlinks = '\033]8;{};{}\033\\{}\033]8;;\033\\'
+        return hyperlinks.format("", url, hyper_txt)
 
 
 if __name__ == "__main__":
@@ -128,24 +137,23 @@ if __name__ == "__main__":
     main entry of the program
     """
 
+    with open("./test.txt", "r") as f:
+        read_std_lines = f.read()
+
+    result = subprocess.run(
+        [
+            'shorten_url',
+            "--update-message",
+            "--url-min-length=100",
+            read_std_lines
+        ],
+        stdout=subprocess.PIPE
+    )
+    sys.stdout.write(result.stdout.decode('utf-8').strip())
+
     home = os.path.expanduser("~")
     filename = os.path.join(home, ".config/neomutt/test_addressbook")
-    address_book_dict = read_address_book(filename)
-    read_std_lines = sys.stdin.buffer.read().decode("utf-8", 'ignore')
-
-    try:
-        result = subprocess.run(
-            [
-                'shorten_url',
-                "--update-message",
-                "--url-min-length=100",
-                read_std_lines
-            ],
-            stdout=subprocess.PIPE
-        )
-        sys.stdout.write(result.stdout.decode('utf-8').strip())
-    except:
-        sys.stdout.write(read_std_lines)
-
-    email_list = read_email(read_std_lines)
-    update_address_book(filename, address_book_dict, email_list, APPEND_MODE)
+    abook_class = AddressBook(filename)
+    abook_class.read_address_book()
+    if email_list := abook_class.read_email(read_std_lines):
+        abook_class.update_address_book(email_list, force_write=True)
